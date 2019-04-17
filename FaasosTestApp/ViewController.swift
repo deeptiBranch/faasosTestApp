40fe51008ca6424fbb618e63879dbd0dc9a36ce2
deeptiBranch <dpandey@branch.io>
2019-04-17 23:24:03 +0530

    Faasos Deeplinking basic

---
+++ FaasosTestApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FaasosTestApp
+//
+//  Created by Deepti Pandey on 17/04/19.
+//  Copyright © 2019 Deepti Pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var dataLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+}
---
+++ Pods/Target Support Files/Pods-FaasosTestApp/Pods-FaasosTestApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FaasosTestApp : NSObject
+@end
+@implementation PodsDummy_Pods_FaasosTestApp
+@end
